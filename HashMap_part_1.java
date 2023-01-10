42161a8862a107ad091d613d9cab5960b91a65e7
Tushar gupta <94210208+tushargCS@users.noreply.github.com>
2023-01-10 10:19:14 +0530

    Create HashMap_part_1.java
    Some_simple_methods

---
+++ HashMap_part_1.java
@@ -0,0 +1,93 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	   
+	   
+	   
+	   //   HashMap<Integer,String> Map = new HashMap<>();                       
+	   //   Map.put(1,"Red");
+	   //   Map.put(2,"black");                                  
+	   //   Map.put(3,"Green");
+	   //   Map.put(4,"white");
+	   //   Map.put(5,"Blue");
+	   //  for ( Map.Entry x : Map.entrySet() ){
+	   //      System.out.println(x.getKey()+"  "+x.getValue());
+	   //  } 
+
+// 	*******************************************************************************************************************************   
+
+	   //  HashMap<Integer,String> Map = new HashMap<>();
+	   //   Map.put(1,"Red");
+	   //   Map.put(2,"black");
+	   //   Map.put(3,"Green");
+	   //   Map.put(4,"white");
+	   //   Map.put(1,"Blue");
+	   //  for ( Map.Entry x : Map.entrySet() ){
+	   //      System.out.println(x.getKey()+"  "+x.getValue());
+	   //  } 
+	 
+// 	 *******************************************************************************************************************************  
+	   
+	   //   HashMap<Integer,String> Map = new HashMap<>();
+	   //   Map.put(1,"Red");
+	   //   Map.put(2,"black");
+	   //   Map.put(3,"Green");
+	   //   Map.put(4,"white");
+	   //   Map.put(1,"Blue");
+	   //   System.out.println("Size of the Hash-Map: "+Map.size());
+	   
+// 	 ********************************************************************************************************************************
+
+       //   HashMap<Integer,String> source = new HashMap<>();
+       //       HashMap<Integer,String> destination = new HashMap<>();
+	   //   source.put(1,"Red");
+	   //   source.put(2,"black");
+	   //   source.put(3,"Green");
+	   //   source.put(4,"white");
+	   //   source.put(1,"Blue");
+	   //   System.out.println("values of the source Hash-Map: "+source);
+	   
+	   //  destination.put(4,"white");
+	   //  destination.put(5,"blue");
+	   //  destination.put(6,"orange");
+	     
+	   //  System.out.println("values of the destination Hash-Map"+destination);
+	     
+	   //  destination.putAll(source);
+	     
+	   //   System.out.println("values of the destination Hash-Map"+destination);
+	   
+//    ********************************************************************************************************************************
+          
+       //   HashMap<Integer,String> Map = new HashMap<>();
+	   //   Map.put(1,"Red");
+	   //   Map.put(2,"black");
+	   //   Map.put(3,"Green");
+	   //   Map.put(4,"white");
+	   //   Map.put(1,"Blue");
+	   //   System.out.println("values of the Hash-Map: "+Map);
+	   //   Map.clear();
+      //    System.out.println("values of the Hash-Map: "+Map);
+      
+//   ********************************************************************************************************************************
+	  
+	  
+       //   HashMap<Integer,String> Map = new HashMap<>();
+	   //   Map.put(1,"Red");
+	   //   Map.put(2,"black");
+	   //   Map.put(3,"Green");
+	   //   Map.put(4,"white");
+	   //   Map.put(1,"Blue");
+	   //   System.out.println("is-Empty: "+Map.isEmpty());
+	   //   Map.clear();
+      //    System.out.println("is-Empty: "+Map.isEmpty());	    
+
+//   ********************************************************************************************************************************
+        
+        
+      
+      
+       
+	}
+}
